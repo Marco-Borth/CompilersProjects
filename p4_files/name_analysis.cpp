77144764e7a8fef00ee78168d28d12c986048466
Marco-Borth <borthmarco@ku.edu>
2021-03-15 21:50:09 -0500

    some name analysis methods defined. varDecl, fnDecl

--- p4_files/name_analysis.cpp
+++ p4_files/name_analysis.cpp
@@ -4,7 +4,7 @@
 
 namespace crona{
 
-//TODO here is a subset of the nodes needed to do nameAnalysis, 
+//TODO here is a subset of the nodes needed to do nameAnalysis,
 // you should add the rest to allow for a complete treatment
 // of any AST
 
@@ -15,6 +15,9 @@ bool ASTNode::nameAnalysis(SymbolTable * symTab){
 
 bool ProgramNode::nameAnalysis(SymbolTable * symTab){
 	bool res = true;
+	ScopeTable * Scope = new ScopeTable();
+
+	symTab->push_front(Scope);
 	for (auto global : *myGlobals){
 		res = global->nameAnalysis(symTab) && res;
 	}
@@ -23,20 +26,81 @@ bool ProgramNode::nameAnalysis(SymbolTable * symTab){
 
 bool VarDeclNode::nameAnalysis(SymbolTable * symTab){
 	bool nameAnalysisOk = true;
+	SemSymbol * varSymbol = new SemSymbol();
+
+	varSymbol->push_front(myType);
+	mySymbol = varSymbol;
+
+	for (auto symbol : *symTab->myFront) {
+		if (ID() == symbol->myID) {
+			nameAnalysisOk = false;
+		}
+	}
+
+	if (nameAnalysisOk) {
+		symTab->myFront->insert(myID, varSymbol);
+	} else {
+		throw new ToDoError("[DELETE ME] I'm a varDecl"
+			" you should add the information from my"
+			" subtree to the symbolTable as a new"
+			" entry in the current scope table"
+		);
+	}
+
+	if (mySymbol == nullptr) {
+
+	}
+	return nameAnalysisOk;
+}
+
+bool formalDeclNode::nameAnalysis(SymbolTable * symTab){
+	bool nameAnalysisOk = true;
+	/*
 	throw new ToDoError("[DELETE ME] I'm a varDecl"
-		" you should add the information from my"	
-		" subtree to the symbolTable as a new"	
+		" you should add the information from my"
+		" subtree to the symbolTable as a new"
 		" entry in the current scope table"
 	);
+	*/
 	return nameAnalysisOk;
 }
 
 bool FnDeclNode::nameAnalysis(SymbolTable * symTab){
 	bool nameAnalysisOk = true;
+	bool formalAnalysisOk = true;
+	bool stmtAnalysisOk = true;
+	SemSymbol * fnSymbol;
+
+	fnSymbol->push_front(myRetType);
+
+	for (auto formal : *myFormals){
+		fnSymbol->push_front(myType);
+	}
+
+	ScopeTable * Scope = new ScopeTable();
+
+	symTab->push_front(Scope);
+
+	for (auto formal : *myFormals){
+		formalAnalysisOk = formal->nameAnalysis(symTab) && formalAnalysisOk;
+	}
+
+	stmtAnalysisOk = formalAnalysisOk && stmtAnalysisOk;
+
+	if (stmtAnalysisOk) {
+		for (auto stmt : *myBody) {
+			stmtAnalysisOk = stmt->nameAnalysis(symTab) && stmtAnalysisOk;
+		}
+	}
+
+	nameAnalysisOk = stmtAnalysisOk && nameAnalysisOk;
+	/*
 	throw new ToDoError("[DELETE ME] I'm an fnDecl."
-		" you should add and make current a new"	
+		" you should add and make current a new"
 		" scope table for my body"
 	);
+	*/
 	return nameAnalysisOk;
 }
+
 }
