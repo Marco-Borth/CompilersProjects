e9e4a9168a6183fcc8bc5c067d8ce51d1697fc47
Marco-Borth <borthmarco@ku.edu>
2021-03-17 21:58:22 -0500

    Project Stop here. Formal Decls not conducting name analysis correctly, may work late for completion

--- p4_files/unparse.cpp
+++ p4_files/unparse.cpp
@@ -308,35 +308,19 @@ void IDNode::unparse(std::ostream& out, int indent){
 	if(mySymbol != nullptr) {
 		out << "(";
 		if (mySymbol->isFn() == true) {
+			/*
 			if (mySymbol->returnList() != nullptr) {
 				for(auto type : *mySymbol->returnList()) {
 					type->unparse(out, 0);
 					out << ", ";
 				}
 			}
+			*/
 			out <<"->";
 		}
 		mySymbol->getType()->unparse(out, 0);
 		out << ")";
 	}
-	/*
-	if(mySymbol != nullptr) {
-		out << "(";
-
-		if (mySymbol->getReturnType() != nullptr) {
-			for(auto type : *mySymbol->returnList()) {
-				type->unparse(out, 0);
-				out << ", ";
-			}
-			out <<"->";
-			mySymbol->getType()->unparse(out, 0);
-
-			mySymbol->getType()->unparse(out, 0);
-			out << ")";
-		} else {
-		}
-	}
-	*/
 }
 
 void HavocNode::unparse(std::ostream& out, int indent){
